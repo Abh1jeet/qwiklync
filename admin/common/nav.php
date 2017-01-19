b391f5e0e63ff2d3de6e2eb293c7a10f98527f0e
Abhijeet pandey <itsmeabhijeetpandey@gmail.com>
2017-01-18 23:23:49 -0800

    start of the project

---
+++ admin/common/nav.php
@@ -0,0 +1,64 @@
+<div class="fixed-left">
+    <div id="wrapper">
+        <div class="topbar">
+            <div class="topbar-left">
+                <div class="text-center">
+                    <a href="index.html" class="logo"><img src="assets/images/logo.png" height="40"></a>
+                    <a href="index.html" class="logo-sm"><img src="assets/images/logo.png" height="36"></a>
+                </div>
+            </div>
+            <div class="navbar navbar-default" role="navigation">
+                <div class="container">
+                    <div class="">
+                        
+                        <ul class="nav navbar-nav navbar-right pull-right">
+                            <li class="dropdown hidden-xs">
+                                <a href="index.html#" data-target="#" class="dropdown-toggle waves-effect waves-light" data-toggle="dropdown" aria-expanded="true"> <i class="fa fa-bell"></i> <span class="badge badge-xs badge-danger">3</span> </a>
+                                <ul class="dropdown-menu dropdown-menu-lg">
+                                    <li class="text-center notifi-title">Notification <span class="badge badge-xs badge-success">3</span></li>
+                                    <li class="list-group">
+                                        <a href="javascript:void(0);" class="list-group-item">
+                                            <div class="media">
+                                                <div class="media-heading">Your order is placed</div>
+                                                <p class="m-0"> <small>Dummy text of the printing and typesetting industry.</small></p>
+                                            </div>
+                                        </a>
+                                        <a href="javascript:void(0);" class="list-group-item">
+                                            <div class="media">
+                                                <div class="media-body clearfix">
+                                                    <div class="media-heading">New Message received</div>
+                                                    <p class="m-0"> <small>You have 87 unread messages</small></p>
+                                                </div>
+                                            </div>
+                                        </a>
+                                        <a href="javascript:void(0);" class="list-group-item">
+                                            <div class="media">
+                                                <div class="media-body clearfix">
+                                                    <div class="media-heading">Your item is shipped.</div>
+                                                    <p class="m-0"> <small>It is a long established fact that a reader will</small></p>
+                                                </div>
+                                            </div>
+                                        </a>
+                                        <a href="javascript:void(0);" class="list-group-item"> <small class="text-primary">See all notifications</small> </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            
+                            <li class="dropdown">
+                                <a href="" class="dropdown-toggle profile waves-effect waves-light" data-toggle="dropdown" aria-expanded="true"><img src="assets/images/users/avatar-1.jpg" alt="user-img" class="img-circle"> </a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="javascript:void(0)"> Profile</a></li>
+                                    <li><a href="javascript:void(0)"><span class="badge badge-success pull-right">5</span> Settings </a></li>
+                                    <li><a href="javascript:void(0)"> Lock screen</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="javascript:void(0)"> Logout</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+	</div>
+</div>
---
+++ admin/common/sideNavBar.php
@@ -0,0 +1,76 @@
+
+<div class="fixed-left">
+	<div id="wrapper">
+		<div class="left side-menu">
+			<div class="sidebar-inner slimscrollleft">
+				<div class="user-details">
+					<div class="text-center"> <img src="assets/images/users/avatar-1.jpg" alt="" class="img-circle"></div>
+					<div class="user-info">
+						<div class="dropdown"> <a href="index.html#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Kenny Rigdon</a>
+							<ul class="dropdown-menu">
+								<li><a href="javascript:void(0)"> Profile</a></li>
+								<li><a href="javascript:void(0)"> Settings</a></li>
+								<li><a href="javascript:void(0)"> Lock screen</a></li>
+								<li class="divider"></li>
+								<li><a href="javascript:void(0)"> Logout</a></li>
+							</ul>
+						</div>
+						<p class="text-muted m-0"><i class="fa fa-dot-circle-o text-success"></i> Online</p>
+					</div>
+				</div>
+				
+				
+			
+				<div id="sidebar-menu">
+						<ul>
+							<li> <a href="../admin/index.php" class="waves-effect"><i class="ti-home"></i><span> Dashboard </span></a></li>
+				
+							
+							<li class="has_sub"> <a href="javascript:void(0);" class="waves-effect"><i class="ion-ios7-people"></i><span> Users </span> <span class="pull-right"><i class="mdi mdi-plus"></i></span></a>
+								<!--<ul class="list-unstyled">
+					
+								</ul> -->
+							</li>
+							
+							<li class="has_sub"> <a href="javascript:void(0);" class="waves-effect"><i class="ion-person-stalker"></i><span> Job </span> <span class="pull-right"><i class="mdi mdi-plus"></i></span></a>
+								<ul class="list-unstyled">
+									<li><a href="../admin/viewCategoryJob.php">View Category Jobs</a></li>
+									<li><a href="../admin/viewAllJobs.php">View All Jobs</a></li>
+									<li><a href="../admin/deleteJob.php">Delete Job</a></li>
+								</ul>
+							</li>
+							
+							<li class="has_sub"> <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-archive"></i><span> Company </span> <span class="pull-right"><i class="mdi mdi-plus"></i></span></a>
+								<!--<ul class="list-unstyled">
+									
+								</ul>-->
+							</li>
+							
+							<li class="has_sub"> <a href="javascript:void(0);" class="waves-effect"><i class="ti-pencil-alt"></i><span> Blog </span> <span class="pull-right"><i class="mdi mdi-plus"></i></span></a>
+								<ul class="list-unstyled">
+									<li><a href="../admin/addBlog.php">Add Blog</a></li>
+									<li><a href="../admin/updateBlog.php">Update Blog</a></li>
+									<li><a href="../admin/deleteBlog.php">Delete Blog</a></li>
+									<li><a href="../admin/viewAllBlog.php">View Blog</a></li>
+								</ul>
+							</li>
+							
+							<li class="has_sub"> <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-clipboard-text"></i><span> FAQ </span> <span class="pull-right"><i class="mdi mdi-plus"></i></span></a>
+								<!--<ul class="list-unstyled">
+									
+								</ul>-->
+							</li>
+							
+							<li class="has_sub"> <a href="javascript:void(0);" class="waves-effect"><i class="ion-chatbubble-working"></i><span> Queries </span> <span class="pull-right"><i class="mdi mdi-plus"></i></span></a>
+								<ul class="list-unstyled">
+									<li><a href="../admin/viewQueries.php">View Queries</a></li>
+									<li><a href="../admin/deleteQueries.php">Delete Queries</a></li>
+									
+								</ul>
+							</li>
+						</ul>
+					</div>
+			</div>
+		</div>
+	</div>
+</div>	
